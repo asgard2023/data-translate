b94c6d3794880f7ae033ceaac0d961b3b08a45a4
chenjh <>
2022-09-23 07:57:16 +0800

    version

--- dataTranslate/src/main/java/cn/org/opendfl/translate/config/DataTranslateConfiguration.java
+++ dataTranslate/src/main/java/cn/org/opendfl/translate/config/DataTranslateConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "translate")
 @Slf4j
 public class DataTranslateConfiguration {
+    private String version = "1.3";
     /**
      * 用于/dflsystem/trTransType/transCounts接口的密码验证
      */
